e1e7eaff3029ec2b625f4859a670ae1ac967b2eb
hector.romang <hector.romang@durango.gob.mx>
2025-05-16 16:49:39 -0600

    Avance problema 20

--- src/main/java/org/roman/easy/ValidParentheses20.java
+++ src/main/java/org/roman/easy/ValidParentheses20.java
@@ -8,39 +8,78 @@ public class ValidParentheses20 {
     public static void main(String[] args) {
 
         ValidParentheses20 obj = new ValidParentheses20();
-        System.out.println(obj.isValid("()"));
+        System.out.println("case 1: " + obj.isValid("()") + " ✅");
+        System.out.println("case 2: " + obj.isValid("()[]{}")+ " ✅");
+        System.out.println("case 3: " + obj.isValid("(]") + " 🚫");
+        System.out.println("case 4: " + obj.isValid("([])") + " ✅");
+        System.out.println("case 5: " + obj.isValid("([)]")+ " 🚫");
+        System.out.println("case 6: " + obj.isValid("((") + " 🚫");
+        System.out.println("case 7: " + obj.isValid("(){}}{")+ " 🚫");
+        System.out.println("case 8: " + obj.isValid("({{{{}}}))")+ " 🚫");
 
     }
 
     public boolean isValid(String s) {
 
+        int loopTimes = s.length();
+
         int loop = 0;
         int antiLoop = s.length() - 1;
+        int indexFor = 0;
 
-        String currentBrackets;
-        String lastBrackets = "";
-        StringBuilder bracketsTogether = new StringBuilder();
+        char currentBrackets;
+        char nextBrackets = 0;
+        char lastBrackets = 0;
+
         Map<Character, Character> map = new HashMap<>();
         map.put('(',')');
         map.put('{','}');
         map.put('[',']');
 
         boolean isClosed = false;
+        boolean extremes = false;
+        boolean continuation = false;
 
         if(s.isEmpty()) {
             return false;
         };
 
-        while(loop < s.length() ){
-            currentBrackets = String.valueOf(s.charAt(loop));
+        if(s.charAt(s.length() - 1) == '{' || s.charAt(s.length() - 1) == '[' || s.charAt(s.length() - 1) == '(') {
+            return false;
+        }
+
+        while(loop < loopTimes ){
+
+            currentBrackets = s.charAt(loop);
+
             if(antiLoop != 0) {
-                lastBrackets = String.valueOf(s.charAt(antiLoop));
+                lastBrackets = s.charAt(antiLoop);
+            }
+            if(loop + 1 < s.length()) {
+                nextBrackets = s.charAt(loop + 1);
             }
 
-            bracketsTogether.append(currentBrackets).append(lastBrackets);
+            for (Map.Entry<Character, Character> entry : map.entrySet()) {
+                if(entry.getKey().equals(currentBrackets) && !entry.getValue().equals(lastBrackets)){
+                    isClosed = false;
+                    extremes = false;
+                } else {
+                    extremes = true;
+                }
+                if (entry.getKey().equals(currentBrackets) && entry.getValue().equals(nextBrackets)) {
+                    isClosed = true;
+                    continuation = true;
+                }else {
+                    continuation = false;
+                }
 
-            System.out.println(bracketsTogether);
+                if(!continuation && !extremes) {
+                    return false;
+                }
 
+                indexFor++;
+                if(indexFor == 2) { break; }
+            }
             loop++;
             antiLoop--;
         }
